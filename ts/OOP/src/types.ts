95a6fee7f2bdc90433f6408324719c16573cc9dd
jordan h <jharrison1@qa.com>
2026-05-21 11:56:20 +0100

    type/interface

---
+++ ts/OOP/src/types.ts
@@ -0,0 +1,38 @@
+// type Trainer = {
+//     name: string;
+//     age: number;
+//     specialty: string;
+//     introduce(): void;
+// }
+
+interface Trainer {
+    name: string;
+    age: number;
+}
+
+interface Trainer {
+    specialty: string;
+    introduce(): void;
+}
+
+
+
+
+const me: Trainer = {
+    name: "JH",
+    age: 32,
+    specialty: "Java",
+    introduce() {
+        console.log(`Hi, I'm ${this.name}, a ${this.age}-year-old trainer specializing in ${this.specialty}.`);
+    }
+}
+
+const cg: Trainer = {
+    name: "Cameron",
+    age: 33,
+    specialty: "Web",
+    introduce() {
+        console.log(`Hi, I'm ${this.name}, a ${this.age}-year-old trainer specializing in ${this.specialty}.`);
+    }
+}
+
