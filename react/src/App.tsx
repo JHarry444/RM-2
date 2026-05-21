31b2a272da46f0213b04ce4b1258a808bb318457
jordan h <jharrison1@qa.com>
2026-05-21 14:51:59 +0100

    react demo

---
+++ react/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <ul>
+
+        <li>Eggs</li>
+        <li>Milk</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
